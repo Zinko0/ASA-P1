9419004dee6e574f7cd9624c97761d96fcd07afd
Rodrigo Zink <rodrigo.zink@tecnico.ulisboa.pt>
2024-11-19 21:56:51 +0000

    codigo iguess

--- projeto.cpp
+++ projeto.cpp
@@ -1,40 +1,61 @@
 #include <stdio.h>
 #include <vector>
+#include <utility>
 
 using namespace std;
 
 typedef vector<vector<int>> matrix;
-void algoritmo(matrix m, vector expressao);
+char* algoritmo(matrix m, vector<int> expressao, int resultado);
 
 int main(void){
 
     int n, n_numbers; // n = lado da matriz, n_numbers = quantidade de números na expressao
+    int resultado;
+    vector<int> expressao(n_numbers);
+    
     scanf("%d %d", &n, &n_numbers);
-
+    
+    vector<vector<pair<int, int>>> v(n, vector<pair<int, int>>(n)); // "hashtable" de n x n
     matrix m(n, vector<int>(n, 0)); // matriz de n x n
     for(int i = 0; i < n; i++){
         for(int j = 0; j < n; j++){
             int a;
             scanf("%d", &a);
-            m[i][j] = a;
+            v[a] = make_pair(i, j);
+            
         }
     }
-    
-    vector<int> expressao(n_numbers);
 
     for (int i = 0; i < n_numbers; i++){
         int a;
         scanf("%d", &a);
         expressao[i] = a;
     }
+    scanf("%d", &resultado);
 
-    algoritmo(m, expressao);
+    algoritmo(m, expressao,resultado);
     //solucao = algoritmo(m, n_numbers);
     //printf(solucao);
 
 }
 
-void algoritmo(matrix m, vector expressao){
+char* algoritmo(matrix m, vector<int> expressao, int resultado){
     
+    if(expressao.size() == 2){ // [2,1]
+        if(m[expressao[0]][expressao[1]] == resultado){
+
+            return "(%d,%d)", expressao[0], expressao[1];
+        }
+        return "",
+    }
+    /*exrpessao [-1]    m[x][1] = 3   |0 - 
+    */
+    if(esquerda){
+        return "(algoritmo(m, expressao, resultado))" + " expressao[-1] )";
+    }
+    if(direita){
+        return "(expressao[0] " + "( algoritmo(m, expressao, resultado))";
+    }
+   
 }
 
