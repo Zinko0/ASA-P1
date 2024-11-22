bc2db7f05efc759a3fa53c7c3cf8c90db1d18159
Saraportugues <saraportugues7@example.com>
2024-11-22 12:33:54 +0000

    creation of matrix

--- projeto.cpp
+++ projeto.cpp
@@ -5,6 +5,8 @@
 using namespace std;
 
 typedef vector<vector<int>> matrix;
+typedef vector<vector<pair<int, int>>> pairmatrix;
+
 char* algoritmo(matrix m, vector<int> expressao, int resultado);
 
 int main(void){
@@ -12,26 +14,32 @@ int main(void){
     int n, n_numbers; // n = lado da matriz, n_numbers = quantidade de números na expressao
     int resultado;
     vector<int> expressao(n_numbers);
+
+    
+    matrix backtracking(n, vector<int>(n, 0));
+    pairmatrix pairMatrix(n, std::vector<std::pair<int, int>>(n, {0, 0}));
     
     scanf("%d %d", &n, &n_numbers);
     
-    vector<vector<pair<int, int>>> v(n, vector<pair<int, int>>(n)); // "hashtable" de n x n
-    matrix m(n, vector<int>(n, 0)); // matriz de n x n
+    matrix m(n, vector<int>(n, 0));
+
     for(int i = 0; i < n; i++){
         for(int j = 0; j < n; j++){
             int a;
             scanf("%d", &a);
-            v[a] = make_pair(i, j);
-            
+            m[i][j] = a;
         }
     }
+
 
     for (int i = 0; i < n_numbers; i++){
         int a;
         scanf("%d", &a);
         expressao[i] = a;
     }
+
     scanf("%d", &resultado);
+
 
     algoritmo(m, expressao,resultado);
     //solucao = algoritmo(m, n_numbers);
