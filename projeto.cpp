ec2a9eb347a958cf6b38ffd13fad69a8bed8b12e
Rodrigo Zink <rodrigo.zink@tecnico.ulisboa.pt>
2024-12-03 19:22:46 +0000

    supostamente funciona

--- projeto.cpp
+++ projeto.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <utility>
 #include <tuple>
+#include <iostream>
 
 using namespace std;
 
@@ -13,33 +14,40 @@ void algoritmo(int resultado, matrix m, vector<int> expressao,int n);
 string backtracking(int i,int j,int result,cube bottomUp, vector<int> expression);
 
 int main(void){
-
+    ios::sync_with_stdio(0);
+    cin.tie(0);
     int n, n_numbers; // n = lado da matriz, n_numbers = quantidade de números na expressao
     int resultado;
     
-    scanf("%d %d", &n, &n_numbers);
+    cin >> n >> n_numbers;
+    
 
     vector<int> expressao(n_numbers);
     matrix m(n, vector<int>(n, 0));
 
     for(int i = 0; i < n; i++){
         for(int j = 0; j < n; j++){
-            int a;
-            scanf("%d", &a);
-            m[i][j] = a;
+            cin >> m[i][j];
         }
     }
 
     for (int i = 0; i < n_numbers; i++){
-        int a;
-        scanf("%d", &a);
-        expressao[i] = a;
+        cin >> expressao[i];
+    }
+    cin >> resultado;
+
+    if(n_numbers == 1){
+        if(expressao[0] == resultado){
+            printf("1\n");
+            printf("%d\n",expressao[0]);
+        }
+        else{
+            printf("0\n");
+        }
+        return 0;
     }
 
-    scanf("%d", &resultado);
-
     algoritmo(resultado, m, expressao ,n_numbers);
-
 }
 
 void algoritmo(int final_result, matrix m, vector<int> expressao, int n){
@@ -52,40 +60,27 @@ void algoritmo(int final_result, matrix m, vector<int> expressao, int n){
     int colocados = 0; //numero de resultados colocados na matriz
     vector<int> resultados(n,0); //vetor para verificar se um certo resultado já foi colocado na matriz
     int resultado;
-    for (int f = 0; f < n; ++f) { //NAO VAMOS AVERIGUAR A CASA [0][n-1] QUE É O RESULTADO
+    for (int f = 0; f < n-1; ++f) { 
         int i = 0;
         int j = f;
         while (j < n) {
             //base condition
-            if(j == i){  //MUITO CUIDADO COM OS INDICES
+            if(j == i){ 
                 bottomUp[i][j].push_back(make_tuple(expressao[i],-1,-1,-1));
             }
-            else if( i != 0 || j != n-1){
+            else{
                 for(int k = j - 1; k >= i; --k){ //começar o algoritmo com o maior K 
-                    for(tuple <int,int,int,int> tuple1 : bottomUp[k+1][j]){ //talvez trocar
-                        for(tuple <int,int,int,int> tuple2 : bottomUp[i][k]){
-                            resultado = m[get<0>(tuple2)-1][get<0>(tuple1)-1];
+                    for(tuple <int,int,int,int> tuple1 : bottomUp[i][k]){ //talvez trocar
+                        for(tuple <int,int,int,int> tuple2 : bottomUp[k+1][j]){
+                            resultado = m[get<0>(tuple1)-1][get<0>(tuple2)-1];
                             if(resultados[resultado-1] == 0){ //se o resultado nao foi colocado
                             // elemento L da casa [i][k] da matriz a (+) elemento R da casa [k+1][j] da matriz e a colocar na casa [i][j] da matriz  
-                                bottomUp[i][j].push_back(make_tuple(resultado, k,get<0>(tuple2),get<0>(tuple1)));
+                                bottomUp[i][j].push_back(make_tuple(resultado, k,get<0>(tuple1),get<0>(tuple2)));
                                 colocados++;
                                 resultados[resultado-1] = 1;
                             }
-                            else if (colocados == n-1){
+                            else if (colocados == n){
                                 k = i-1;
-                            }
-                        }
-                    }
-                }
-            }else{
-                for(int k = j - 1; k >= i; --k){ //começar o algoritmo com o maior K 
-                    for(tuple <int,int,int,int> tuple1 : bottomUp[k+1][j]){ //talvez trocar
-                        for(tuple <int,int,int,int> tuple2 : bottomUp[i][k]){
-                            if(m[get<0>(tuple2)-1][get<0>(tuple1)-1] == final_result){ 
-                                bottomUp[i][j].push_back(make_tuple(final_result,k,get<0>(tuple1), get<0>(tuple2)));
-                                printf("1\n");
-                                printf("(%s)\n",backtracking(0,n-1,final_result,bottomUp,expressao).c_str());
-                                return;
                             }
                         }
                     }
@@ -97,24 +92,35 @@ void algoritmo(int final_result, matrix m, vector<int> expressao, int n){
             ++j;
         }
     }
+    int i = 0, j = n-1;
+    for(int k = j - 1; k >= i; --k){ //começar o algoritmo com o maior K 
+        for(tuple <int,int,int,int> tuple1 : bottomUp[i][k]){ //talvez trocar
+            for(tuple <int,int,int,int> tuple2 : bottomUp[k+1][j]){
+                if(m[get<0>(tuple1)-1][get<0>(tuple2)-1] == final_result){ 
+                    bottomUp[i][j].push_back(make_tuple(final_result,k,get<0>(tuple1), get<0>(tuple2)));
+                    printf("1\n");
+                    printf("%s\n",backtracking(0,n-1,final_result,bottomUp,expressao).c_str());
+                    return;
+                }
+            }
+        }
+    }
     printf("0\n");
     return;
 }
 
 string backtracking(int i,int j,int result,cube bottomUp, vector<int> expression){
-    int left_number;
-    int right_number;
-    int k;
+
     if(i == j){
         return to_string(expression[i]);
     }
     for(tuple<int,int,int,int> f : bottomUp[i][j]){
         if(get<0>(f) == result){
-            left_number = get<2>(f); 
-            right_number = get<3>(f);
-            k = get<1>(f);
-            break;
+            int left_number = get<2>(f); 
+            int right_number = get<3>(f);
+            int k = get<1>(f);
+            return "(" + backtracking(i,k,left_number,bottomUp,expression) + " " + backtracking(k+1,j,right_number,bottomUp,expression) + ")";
         }
     } 
-    return "(" + backtracking(i,k,left_number,bottomUp,expression) + " " + backtracking(k+1,j,right_number,bottomUp,expression) + ")";
+    return "ERROR";
 }
